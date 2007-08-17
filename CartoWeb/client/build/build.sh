f26bac67fc9a7a687da913e75d46d12204b6ab44
fredj <frederic.junod@camptocamp.com>
2007-08-17 13:32:40 +0000

    fix paths (see #8)
    git-svn-id: http://svn.mapfish.org/svn/mapfish/trunk@7 25cca10d-0837-0410-9e4f-92b931fda1bc

--- CartoWeb/client/build/build.sh
+++ CartoWeb/client/build/build.sh
@@ -3,15 +3,15 @@
 HERE=`pwd`
 
 # build dojo and friends
-cd ../htdocs/cwbase/util/buildscripts/
+cd ../cwbase/util/buildscripts/
 sh build.sh profileFile=$HERE/cartoweb.profile.js action=release releaseName=cartoweb copyTests=false
 cd $HERE
 
 
-rm -fr ../htdocs/cwbase/release/cartoweb/openlayers/*
+rm -fr ../cwbase/release/cartoweb/openlayers/*
 
 # patch openlayers
-cd ../htdocs/cwbase/openlayers/
+cd ../cwbase/openlayers/
 
 for file in `ls $HERE/openlayers_patches/`
 do 
@@ -21,15 +21,15 @@ done
 # build openlayers
 
 cd $HERE
-cd ../htdocs/cwbase/openlayers/build/
+cd ../cwbase/openlayers/build/
 python build.py full
-mkdir -p $HERE/../htdocs/cwbase/release/cartoweb/openlayers/
-mv OpenLayers.js $HERE/../htdocs/cwbase/release/cartoweb/openlayers/
+mkdir -p $HERE/../cwbase/release/cartoweb/openlayers/
+mv OpenLayers.js $HERE/../cwbase/release/cartoweb/openlayers/
 
-rm -rf $HERE/../htdocs/cwbase/release/cartoweb/openlayers/img
-cp -r ../img $HERE/../htdocs/cwbase/release/cartoweb/openlayers/
+rm -rf $HERE/../cwbase/release/cartoweb/openlayers/img
+cp -r ../img $HERE/../cwbase/release/cartoweb/openlayers/
 
-mkdir -p $HERE/../htdocs/cwbase/release/cartoweb/openlayers/theme/
-mkdir -p $HERE/../htdocs/cwbase/release/cartoweb/openlayers/theme/default
-cp ../theme/default/style.css $HERE/../htdocs/cwbase/release/cartoweb/openlayers/theme/default/style.css
+mkdir -p $HERE/../cwbase/release/cartoweb/openlayers/theme/
+mkdir -p $HERE/../cwbase/release/cartoweb/openlayers/theme/default
+cp ../theme/default/style.css $HERE/../cwbase/release/cartoweb/openlayers/theme/default/style.css
 
