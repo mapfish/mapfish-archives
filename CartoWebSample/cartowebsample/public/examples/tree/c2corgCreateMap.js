7386b8e13307b46bc4cb05d24bc0cf9de64f6fdc
elemoine <eric.lemoine@camptocamp.com>
2007-10-15 14:49:39 +0000

    add copyright and license headers
    git-svn-id: http://svn.mapfish.org/svn/mapfish/trunk@61 25cca10d-0837-0410-9e4f-92b931fda1bc

--- CartoWebSample/cartowebsample/public/examples/tree/c2corgCreateMap.js
+++ CartoWebSample/cartowebsample/public/examples/tree/c2corgCreateMap.js
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2007  Camptocamp
+ *
+ * This file is part of CartoWeb
+ *
+ * CartoWeb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CartoWeb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CartoWeb.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 function c2corgCreateMap(mapDiv) {
 
     var options = {
