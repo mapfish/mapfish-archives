1aeec36dbdefe84cf33904912938126a425139ff
elemoine <eric.lemoine@camptocamp.com>
2007-10-24 15:10:18 +0000

    javascript autoload
    git-svn-id: http://svn.mapfish.org/svn/mapfish/trunk@117 25cca10d-0837-0410-9e4f-92b931fda1bc

--- MapFish/client/mfbase/mapfish/MapFish.js
+++ MapFish/client/mfbase/mapfish/MapFish.js
@@ -31,5 +31,79 @@
      * The mapfish object provides a namespace for all things 
      */
     window.mapfish = {
+
+        /**
+         * Property: _scriptName
+         * {String} Relative path of this script.
+         */
+        _scriptName: (!singleFile) ? "mapfish/MapFish.js" : "MapFish.js",
+
+        /**
+         * Function: _getScriptLocation
+         * Return the path to this script.
+         *
+         * Returns:
+         * Path to this script
+         */
+        _getScriptLocation: function () {
+            var scriptLocation = "";
+            var scriptName = mapfish._scriptName;
+         
+            var scripts = document.getElementsByTagName('script');
+            for (var i = 0; i < scripts.length; i++) {
+                var src = scripts[i].getAttribute('src');
+                if (src) {
+                    var index = src.lastIndexOf(scriptName); 
+                    // is it found, at the end of the URL?
+                    if ((index > -1) && (index + scriptName.length == src.length)) {  
+                        scriptLocation = src.slice(0, -scriptName.length);
+                        break;
+                    }
+                }
+            }
+            return scriptLocation;
+         }
     };
+
+    /**
+     * mapfish.singleFile is a flag indicating this file is being included
+     * in a Single File Library build of the MapFish Library.
+     * 
+     * When we are *not* part of a SFL build we dynamically include the
+     * MapFish library code.
+     * 
+     * When we *are* part of a SFL build we do not dynamically include the 
+     * MapFish library code as it will be appended at the end of this file.
+      */
+    if(!singleFile) {
+        var jsfiles = new Array(
+            "core/Color.js",
+            "core/GeoStat.js",
+            "core/Util.js",
+            "widgets/MapComponent.js",
+            "widgets/Shortcuts.js",
+            "widgets/geostat/Choropleth.js",
+            "widgets/tree/LayerTree.js"
+        ); // etc.
+
+        var allScriptTags = "";
+        var host = mapfish._getScriptLocation() + "mapfish/";
+    
+        for (var i = 0; i < jsfiles.length; i++) {
+            if (/MSIE/.test(navigator.userAgent) || /Safari/.test(navigator.userAgent)) {
+                var currentScriptTag = "<script src='" + host + jsfiles[i] + "'></script>"; 
+                allScriptTags += currentScriptTag;
+            } else {
+                var s = document.createElement("script");
+                s.src = host + jsfiles[i];
+                var h = document.getElementsByTagName("head").length ? 
+                           document.getElementsByTagName("head")[0] : 
+                           document.body;
+                h.appendChild(s);
+            }
+        }
+        if (allScriptTags) {
+            document.write(allScriptTags);
+        }
+    }
 })();
