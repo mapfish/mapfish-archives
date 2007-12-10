8a094be0a2b74b39c9a2b2190a4989d5c0f9a82d
spasche <spasche>
2007-12-10 14:45:59 +0000

    trailing space removal
    git-svn-id: http://svn.mapfish.org/svn/mapfish/trunk@212 25cca10d-0837-0410-9e4f-92b931fda1bc

--- MapFishSample/mapfishsample/public/examples/tree/c2corgCreateMap.js
+++ MapFishSample/mapfishsample/public/examples/tree/c2corgCreateMap.js
@@ -22,15 +22,15 @@ function c2corgCreateMap(mapDiv) {
 
     var options = {
         projection: "EPSG:4326",
-        controls: [new OpenLayers.Control.MouseDefaults()] , 
+        controls: [new OpenLayers.Control.MouseDefaults()],
         'numZoomLevels': 20
     };
 
     var map = new OpenLayers.Map(mapDiv , options);
-    
+
     var wms = new OpenLayers.Layer.WMS("OpenLayers WMS", 
         "http://labs.metacarta.com/wms/vmap0",
-        {layers: 'basic'}, 
+        {layers: 'basic'},
         {isBaseLayer: true}
     );
     wms.setVisibility(false);
@@ -47,7 +47,7 @@ function c2corgCreateMap(mapDiv) {
     );
     twms.setVisibility(false);
     map.addLayer(twms);
-  
+
     c2cwmsLayers = ['parkings', 'summits', 'refuges', 'sites',
                     'countries', 'departements_fr', 'cantons_ch',
                     'massifs'];
@@ -61,7 +61,7 @@ function c2corgCreateMap(mapDiv) {
         }
     );
     c2cwms.setVisibility(false);
-    map.addLayer(c2cwms);  
+    map.addLayer(c2cwms);
 
     map.setCenter(new OpenLayers.LonLat(5,45), 6);
     map.addControl(new OpenLayers.Control.PanZoomBar());
