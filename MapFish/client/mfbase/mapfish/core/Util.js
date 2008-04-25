b92f472be4f7f5ead9eec977fe44fb31c4ce688b
fvanderbiest <franois.vanderbiest@camptocamp.com>
2008-04-25 08:36:30 +0000

    fixed #111 : added mapfish.Util.getIconUrl in order to retrieve layerTree icons
    git-svn-id: http://svn.mapfish.org/svn/mapfish/trunk@462 25cca10d-0837-0410-9e4f-92b931fda1bc

--- MapFish/client/mfbase/mapfish/core/Util.js
+++ MapFish/client/mfbase/mapfish/core/Util.js
@@ -31,3 +31,42 @@ mapfish.Util.max = function(array) {
 mapfish.Util.min = function(array) {
     return Math.min.apply({}, array);
 };
+
+/**
+ * Function: getIconUrl
+ * Builds the URL for a layer icon, based on a WMS GetLegendGraphic request.
+ * 
+ * Parameters:
+ * wmsUrl - {String} The URL of a WMS server.
+ * options - {Object} The options to set in the request:
+ *                    'layer' - the name of the layer for which the icon is requested (required)
+ *                    'rule' - the name of a class for this layer (this is set to the layer name if not specified)
+ *                    'format' - "image/png" by default
+ *                    ... 
+ * 
+ * Returns:
+ * {String} The URL at which the icon can be found.
+ */
+mapfish.Util.getIconUrl = function(wmsUrl, options) {
+    if (!options.layer) {
+        OpenLayers.Console.warn(
+            'Missing required layer option in mapfish.Util.getIconUrl');
+        return '';
+    }
+    if (!options.rule) {
+        options.rule = options.layer;
+    }
+    var url = wmsUrl + ((wmsUrl.indexOf('?') != (wmsUrl.length - 1)) ? '?' : '');
+    var options = OpenLayers.Util.extend({
+        layer: "",
+        rule: "",
+        service: "WMS",
+        version: "1.1.1",
+        request: "GetLegendGraphic",
+        format: "image/png",
+        width: 16,
+        height: 16
+    }, options);
+    options = OpenLayers.Util.upperCaseObject(options);
+    return url + OpenLayers.Util.getParameterString(options);
+}
