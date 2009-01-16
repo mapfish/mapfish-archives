cebc4aa2586022d3f0e758a5ecee540457e35c22
elemoine <eric.lemoine@camptocamp.com>
2009-01-16 09:47:38 +0000

    MapFishSample now requires Pylons>=0.9.6.2,<=0.9.7.99
    git-svn-id: http://svn.mapfish.org/svn/mapfish/tags/1.1@1420 25cca10d-0837-0410-9e4f-92b931fda1bc

--- MapFishSample/setup.py
+++ MapFishSample/setup.py
@@ -12,7 +12,7 @@ setup(
     #author="",
     #author_email="",
     #url="",
-    install_requires=["Pylons>=0.9.5"],
+    install_requires=["Pylons>=0.9.6.2,<=0.9.7.99"],
     packages=find_packages(exclude=['ez_setup']),
     include_package_data=True,
     test_suite='nose.collector',
