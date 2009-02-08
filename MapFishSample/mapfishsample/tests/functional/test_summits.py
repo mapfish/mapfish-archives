fe2e82c515acec46dc88844e16c5850470791d34
elemoine <eric.lemoine@camptocamp.com>
2009-02-08 15:34:31 +0000

    add some functional tests for the summits controller, the tests show that changesets 1625 and 1626 broke the "box" filter, no functional change
    git-svn-id: http://svn.mapfish.org/svn/mapfish/trunk@1636 25cca10d-0837-0410-9e4f-92b931fda1bc

--- MapFishSample/mapfishsample/tests/functional/test_summits.py
+++ MapFishSample/mapfishsample/tests/functional/test_summits.py
@@ -1,6 +1,72 @@
 from mapfishsample.tests import *
+from geojson import loads
 
 class TestSummitsController(TestController):
     def test_index(self):
         response = self.app.get(url_for(controller='summits'))
-        # Test response...
+        assert "FeatureCollection" in response
+
+    def test_index_limit_offset(self):
+        # limit
+        params = {"limit": 10}
+        response = self.app.get(url_for(controller='summits'),
+                                params=params)
+        geojson = loads(response.response._body)
+        features = geojson["features"]
+        assert len(features) == 10
+
+        fid1 = features[1]["id"]
+
+        # limit & offset
+        params = {"limit": 10, "offset": 1}
+        response = self.app.get(url_for(controller='summits'),
+                                params=params)
+        geojson = loads(response.response._body)
+        features = geojson['features']
+        assert len(features) == 10
+        assert features[0]["id"] == fid1
+
+    def test_index_lonlat(self):
+        params = {"lon": 5, "lat": 45, "tolerance": 0.5}
+        response = self.app.get(url_for(controller='summits'),
+                                params=params)
+        geojson = loads(response.response._body)
+        features = geojson["features"]
+        for f in features:
+            coords = f["geometry"]["coordinates"]
+            assert abs(coords[0] - 5) <= 0.5 
+            assert abs(coords[1] - 45) <= 0.5
+
+    def test_index_box(self):
+        # changeset 1625 and 1626 make this test fail
+        params = {"box": "5,45,5.5,45.5"}
+        response = self.app.get(url_for(controller='summits'),
+                                params=params)
+        geojson = loads(response.response._body)
+        features = geojson["features"]
+        for f in features:
+            coords = f["geometry"]["coordinates"]
+            assert coords[0] >= 5
+            assert coords[1] >= 45
+            assert coords[0] <= 5.5
+            assert coords[1] <= 45.5
+
+    def test_index_min(self):
+        params = {"limit": 5, "min": 2000}
+        response = self.app.get(url_for(controller='summits'),
+                                params=params)
+        geojson = loads(response.response._body)
+        features = geojson["features"]
+        for f in features:
+            elevation = f["properties"]["elevation"]
+            assert elevation >= 2000
+
+    def test_index_max(self):
+        params = {"limit": 5, "max": 2000}
+        response = self.app.get(url_for(controller='summits'),
+                                params=params)
+        geojson = loads(response.response._body)
+        features = geojson["features"]
+        for f in features:
+            elevation = f["properties"]["elevation"]
+            assert elevation <= 2000
